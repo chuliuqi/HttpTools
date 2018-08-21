a8bd8df2061ab1a3ff8e17a1c3c2272370c92dce
liuqi.chu <liuqi.chu@ikang.com>
2018-08-21 11:13:28 +0800

    添加项目文件。

---
+++ HttpTool/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace HttpTool
+{
+    partial class FrmHttpTool
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Body = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResonse = new System.Windows.Forms.TextBox();
+            this.btnSender = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL：";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(43, 15);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(518, 21);
+            this.txtURL.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "HTTPGET",
+            "HTTPPOST",
+            "HTTPINPUT",
+            "HTTPDELETE"});
+            this.comboBox1.Location = new System.Drawing.Point(614, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(560, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Type：";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btnSender);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(7, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(736, 65);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.txtContent);
+            this.panel2.Controls.Add(this.Body);
+            this.panel2.Location = new System.Drawing.Point(7, 78);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(736, 174);
+            this.panel2.TabIndex = 4;
+            // 
+            // Body
+            // 
+            this.Body.AutoSize = true;
+            this.Body.Location = new System.Drawing.Point(7, 4);
+            this.Body.Name = "Body";
+            this.Body.Size = new System.Drawing.Size(41, 12);
+            this.Body.TabIndex = 0;
+            this.Body.Text = "Body：";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(9, 20);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(719, 142);
+            this.txtContent.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Controls.Add(this.txtResonse);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(7, 258);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(736, 161);
+            this.panel3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Reponse：";
+            // 
+            // txtResonse
+            // 
+            this.txtResonse.Location = new System.Drawing.Point(7, 25);
+            this.txtResonse.Multiline = true;
+            this.txtResonse.Name = "txtResonse";
+            this.txtResonse.ReadOnly = true;
+            this.txtResonse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResonse.Size = new System.Drawing.Size(721, 126);
+            this.txtResonse.TabIndex = 1;
+            // 
+            // btnSender
+            // 
+            this.btnSender.Location = new System.Drawing.Point(7, 35);
+            this.btnSender.Name = "btnSender";
+            this.btnSender.Size = new System.Drawing.Size(75, 23);
+            this.btnSender.TabIndex = 1;
+            this.btnSender.Text = "HttpSend";
+            this.btnSender.UseVisualStyleBackColor = true;
+            this.btnSender.Click += new System.EventHandler(this.btnSender_Click);
+            // 
+            // FrmHttpTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 421);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmHttpTool";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HttpTool";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Label Body;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtResonse;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSender;
+    }
+}
+
---
+++ HttpTool/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HttpTool
+{
+    public partial class FrmHttpTool : Form
+    {
+        public FrmHttpTool()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSender_Click(object sender, EventArgs e)
+        {
+            string status = string.Empty;
+            txtResonse.Text= HttpClientHelper.GetResponse("http://www.baidu.com", out status);
+            txtResonse.Text = HttpClientHelper.RestfulGet("http://www.baidu.com");
+        }
+    }
+}
